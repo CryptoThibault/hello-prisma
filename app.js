7c0868a8f8d2be73c3791178719cabccaf0ee6b4
CryptoThibault <thibault.chalaoux@gmail.com>
2021-07-26 17:13:42 +0200

    test

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
-// import
 const { PrismaClient } = require('@prisma/client')
-
-// instanciation d'un nouveau prisma client
 const prisma = new PrismaClient()
 
-const main = async () => {
-  // Equivalent d'un SELECT * FROM users;
+async function main() {
   const allUsers = await prisma.users.findMany()
-  // Affichage du résultat
   console.log(allUsers)
+
+  const result = await prisma.users.create({
+    data: {
+      login: 'alice',
+      firstName: 'Alice',
+      lastName: 'Euler',
+      email: 'alice@gmail.com',
+    },
+  })
+  console.log(result)
+
+  const nbInserted = await prisma.users.createMany({
+    data: [
+      {
+        login: 'bob',
+        firstName: 'Bob',
+        lastName: 'Durac',
+        email: 'bob@mail.com',
+      },
+      {
+        login: 'charlie',
+        firstName: 'Charlie',
+        lastName: 'Turing',
+        email: 'charlie@mail.com',
+      },
+    ],
+    skipDuplicates: true,
+  })
+  console.log(nbInserted)
+
+  const users = prisma.users.findMany()
+  console.log(users)
 }
 
-// Execution de la fonction main
 main()
   .catch((e) => {
     throw e
   })
   .finally(async () => {
-    // Fermeture de la connection à la fin de l'exécution du script
     await prisma.$disconnect()
   })
